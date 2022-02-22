cdaa51566925e280b3769c03eda52af3ed7022c4
plyeronn <plyeronn@gmail.com>
2022-02-22 17:33:12 +0100

    Transferred Styles in separate Directory

--- src/App.vue
+++ src/App.vue
@@ -57,19 +57,6 @@ export default {
 </script>
 
 <style>
-body {
-  background: url("assets/bg.jpg") no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
 @import "~bootstrap/dist/css/bootstrap.css";
+@import "./styles/styles.css";
 </style>
